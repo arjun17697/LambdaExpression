b0e2868ef9370c11aeee728ec1ddfe0d741c0b43
arjun17697 <arjun17697@gmail.com>
2020-10-03 22:24:38 +0530

    adding uc1 first name validator

---
+++ com/bridgelabz/user/UserRegis.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.user;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegis {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void firstname(String fname) {
+		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+		Matcher matcher = pattern.matcher(fname);
+		if (matcher.matches() == true) {
+			System.out.println("Entered name is valid");
+		} else {
+			System.out.println("Entered name is not valid");
+		}
+	}
+
+	public static void main(String[] args) {
+		// First Name input
+		System.out.println("Please enter first name");
+		String fname = sc.next();
+		firstname(fname);
+
+	}
+}

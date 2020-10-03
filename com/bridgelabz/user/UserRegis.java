3ffe50da83fa13f26e8f36400e7334f4e8159c07
arjun17697 <arjun17697@gmail.com>
2020-10-03 22:39:26 +0530

    adding uc3 email validator

--- com/bridgelabz/user/UserRegis.java
+++ com/bridgelabz/user/UserRegis.java
@@ -7,7 +7,8 @@ import java.util.regex.Pattern;
 public class UserRegis {
 	static Scanner sc = new Scanner(System.in);
 
-	public static void firstname(String fname) {
+	// Method to validate first and last name
+	private static void firstname(String fname) {
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
 		Matcher matcher = pattern.matcher(fname);
 		if (matcher.matches() == true) {
@@ -16,17 +17,36 @@ public class UserRegis {
 			System.out.println("Entered name is not valid");
 		}
 	}
+	
+	private static void email(String mail) {
+		Pattern pattern = Pattern.compile("\"^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$\"");
+		Matcher matcher = pattern.matcher(mail);
+		if (matcher.matches() == true) {
+			System.out.println("Entered email is valid");
+		} else {
+			System.out.println("Entered email is invalid");
+		}
+	}
+			
+	
 
 	public static void main(String[] args) {
 		// First Name input
 		System.out.println("Please enter first name");
 		String fname = sc.next();
 		firstname(fname);
-		
+
 		// Last Name Input
 		System.out.println("Please enter first name");
 		String lname = sc.next();
 		firstname(lname);
+		
+		//Email input
+		System.out.println("Please enter email");
+		String mail = sc.next();
+		email(mail);
 
 	}
+
+	
 }
